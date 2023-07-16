cb63119a0803779947c51e68a2a84e97fbc9689b
ALE <alierisir@gmail.com>
2023-07-16 20:01:31 +0300

    3.3-Data Types

---
+++ sampleProject/3.3-Data Types/index.js
@@ -0,0 +1,11 @@
+let myNumber=5
+let myString="string"
+let myBool=true
+let myVariable
+let myWall={
+    length:15,
+    width:0.3,
+    height:3,
+    material:["paint","plaster","brick"]
+}
+let myList=["ali",5,"izmir",true,{length:15,width:0.3,height:3},[2,4,5,6]]

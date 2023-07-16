cb1e3ad34a10e1ceb22ed2fc3cf7ffa7d33469df
ALE <alierisir@gmail.com>
2023-07-16 22:29:36 +0300

    3.6-Objects

---
+++ sampleProject/3.6-Objects/index.js
@@ -0,0 +1,17 @@
+const person = {
+    name : "Ali",
+    lastName: "Erişir",
+    age : 30,
+    city: "İzmir",
+    country:"Turkey",
+    gsm:"+901234567890",
+    profession:"Civil Engineer"
+}
+
+let keyword = "name"
+
+console.log(person[keyword])
+
+console.log(person["name"])
+
+console.log(person.name)

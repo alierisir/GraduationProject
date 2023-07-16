e1563a10db678ab974148a64d9e8e62edea3a48d
ALE <alierisir@gmail.com>
2023-07-16 21:01:25 +0300

    3.4-Operators

---
+++ sampleProject/3.4-Operators/index.js
@@ -0,0 +1,40 @@
+//Arithmetic Operations
+let x = 10
+let y = 3
+console.log("Arithmetic Operations")
+console.log(x + y)//Add 
+console.log(x - y) //Substract 
+console.log(x * y) //Multiplication 
+console.log(x / y) //Division 
+console.log(x % y) //Module 
+console.log(x ** y) //Exponent 
+console.log("---------------------")
+//Assigning Operations
+let a = 10; // Declare + assign 
+console.log("Assigning Operations")
+a = 5; // Assign 
+console.log(a)
+console.log(a += 5); // Assign and add. Equivalent to: a = a + 5; 
+console.log(a -= 5); // Assign and subtract. Equivalent to: a = a - 5; 
+console.log(a *= 5); // Assign and multiply. Equivalent to: a = a * 5; 
+console.log(a /= 5); // Assign and divide. Equivalent to: a = a / 5; 
+console.log("---------------------")
+//Comparision Operations : Returns boolean values.
+let b = 1;
+console.log("Comparision Operations")
+console.log(b === 2);
+console.log(b !== 2);
+console.log(b < 2);
+console.log(b <= 2);
+console.log(b > 2);
+console.log(b >= 2);
+console.log("---------------------")
+//Logical Operations : Returns boolean values.
+console.log("Logical Operations")
+console.log(false && true); // false
+console.log(false && false); // false
+console.log(true && true); // true
+console.log(false || true); // true
+console.log(false || false); // false
+console.log(true || true); // true
+console.log("---------------------")

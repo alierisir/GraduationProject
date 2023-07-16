93aa9bd53b33af59d0e80b57fcfb6fa3d801e28f
ALE <alierisir@gmail.com>
2023-07-16 16:58:10 +0300

    3.2-Constants and Variables

---
+++ sampleProject/3.2-Constant and Variables/index.js
@@ -0,0 +1,9 @@
+let myAge = 25;    //first declaration of a variable
+console.log(myAge); // Result is 25 in the console.
+myAge = 26;  // reassigned value of a variable
+console.log(myAge); // Result is 26 in the console.
+
+const myName = "Ali";    //first declaration of a constant
+console.log(myName); // Result is “Ali” in the console.
+myName = "Ahmet";  // This reassigning a constant will give an error.
+console.log(myName); // Will not give a result. Program will stop when the above error occurs.

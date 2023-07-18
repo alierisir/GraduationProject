b818f945a1582be9d4a5066ac925c021e8195e16
ALE <alierisir@gmail.com>
2023-07-18 20:48:28 +0300

    5.1-Base IFC Viewer

---
+++ sampleProject/4-Tooling/bimProject/index.js
@@ -0,0 +1,12 @@
+import { Color } from 'three';
+import { IfcViewerAPI } from 'web-ifc-viewer';
+
+const container = document.getElementById('viewer-container');
+const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xffffff) });
+
+// Create grid and axes
+viewer.grid.setGrid();
+viewer.axes.setAxes();
+
+const url="./samples/01.ifc"
+viewer.IFC.loadIfcUrl(url)
